bb8a5cde1f0272a9dc2907f1c9de96d8d931d0f2
Shourya-Coding <78908009+Shourya-Coding@users.noreply.github.com>
2021-11-25 15:42:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+function setup()
+{
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+
+
+function clearCanvas()
+{
+    background("white");
+}
+
+
+function preload(){
+    classifier = ml5.imageClassifier("DoodleNet");
+}
+
+function draw()
+{
+    strokeWeight(16);
+    stroke(0);
+    if(mouseIsPressed){
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+
+
+function classifyCanvas()
+{
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById('label').innerHTML = 'LABEL : ' + results[0].label;
+        
+        document.getElementById('confidence').innerHTML = 'CONFIDENCE : ' + Math.round(results[0].confidence * 100) + '%';
+
+        utter = new SpeechSynthesisUtterance(results[0].label);
+        synth.speak(utter);
+  }
+}
